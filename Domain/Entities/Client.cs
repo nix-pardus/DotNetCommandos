901de10d927b915c5a912e1e53d7b13d0e07ecd3
Дмитрий Марчук <dmitrii.marchuk@indusoft.ru>
2025-08-25 11:26:56 +0300

    Добавьте файлы проекта.

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Клиент
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        /// UID
+        /// </summary>
+        public Guid Id { get; set; }//Я всё-равно не считаю long хорошим выбором для ID
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
