0afe5de109088d7c6a8125db3b98de3dcdf34a0a
nix_pardus <nix_pardus@bk.ru>
2025-08-30 22:01:03 +0300

    Добавил обновление сотрудника

--- Application/Interfaces/IEmployeeService.cs
+++ Application/Interfaces/IEmployeeService.cs
@@ -5,7 +5,7 @@ namespace Application.Interfaces;
 public interface IEmployeeService
 {
     Task CreateAsync(EmployeeDto dto);
-    Task<EmployeeDto> UpdateAsync(EmployeeDto dto);
+    Task UpdateAsync(EmployeeDto dto);
     Task<bool> DeleteAsync(Guid id);
     Task<IEnumerable<EmployeeDto>> GetAllAsync();
 }
